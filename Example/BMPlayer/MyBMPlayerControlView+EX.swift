b794b1b65ccfe620fa1b5398ec0a5b69ca8d686a
tran-ngocdien <tran-ngocdien@AP0808.local>
2017-07-25 18:48:30 +0900

    - create MyBMPlayerControlViewDelegate          MyBMPlayerControlView

---
+++ Example/BMPlayer/MyBMPlayerControlView+EX.swift
@@ -0,0 +1,20 @@
+//
+//  ádasda.swift
+//  BMPlayer
+//
+//  Created by tran-ngocdien on 2017/07/25.
+//  Copyright © 2017 CocoaPods. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension MyBMPlayerControlView {
+    public enum ButtonType: Int {
+        case play       = 101
+        case pause      = 102
+        case back       = 103
+        case fullscreen = 105
+        case replay     = 106
+    }
+}
